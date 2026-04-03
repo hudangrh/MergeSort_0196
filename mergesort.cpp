3703571133652cc84780675ae04e7d80529e1845
hudangrh <hudangrhkuliah@gmail.com>
2026-04-03 21:38:13 +0700

    mergesort7

--- mergesort.cpp
+++ mergesort.cpp
@@ -47,5 +47,18 @@ void merge(int low, int mid, int high)
     /fungsi rekursi -memanggil diri sendiri
     mergesort(low, mid); // step 3.a
     mergesort(mid + 1, high); // step 3.b
-    
+
+    //step4
+    int i = low;   // step 4.a
+    int j = mid+ 1; // step 4.b
+    int k = low;  // step 4.c
+
+    while (i <= mid && j <= high) / step 4.d
+    {
+        if (arr[i] <= arr[j])  // step 4.i
+        {
+            B[k] = arr[i]
+            i++;
+        }
+    }
 }
